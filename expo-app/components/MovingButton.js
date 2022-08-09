391122aa38928c7a9abd55289c61c1cd32a0cbb1
asilverde <sidajay01@gmail.com>
2022-08-09 18:09:38 -0500

    Restructure for redux integration

---
+++ expo-app/components/MovingButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Single file to handle line, box, and circle
+function MovingButton(props) {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default MovingButton
