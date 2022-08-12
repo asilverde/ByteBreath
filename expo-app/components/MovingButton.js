f86afd47442b6a7a5584629336728f97223b2de8
asilverde <sidajay01@gmail.com>
2022-08-11 20:44:04 -0500

    Begin V2 refactor

--- expo-app/components/MovingButton.js
+++ expo-app/components/MovingButton.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react';
 
-// Single file to handle line, box, and circle
+// Single function to handle line, box, and circle breathing
 function MovingButton(props) {
+    useEffect(() => {
+        for (let a = 0; a < props.animations.length() - 1; a++) {
+            Animated.timing(props.animations[a], {
+                toValue: props.animations[a],
+                useNativeDriver: false,
+                duration: props.time[a],
+            }).start();
+        }
+    }, []);
+
     return (
         <div>
         </div>
